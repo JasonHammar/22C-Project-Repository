2110fbba03dff93196462d6d5875934819c90e0b
Daniel Morales <42255317+Dofmor@users.noreply.github.com>
2020-06-10 10:48:37 -0700

    Update VideoGames.java

--- VideoGames.java
+++ VideoGames.java
@@ -1,103 +1,102 @@
-
-public class VideoGames {
+public class VideoGames  implements Comparable<VideoGames>{
 	private String serialNumber;
 	private String title;
 	private int year;
-	private String studioOrPublisher;
+	private String publisher;
 	private String rating;
 	
 
 	/**
-	 * Constructor for the Movie class
+	 * Constructor for the Video Game class
 	 *
-	 * @param title    the Movie's title
-	 * @param director the Movie's director
-	 * @param year     the year the Movie was released
-	 * @param gross    the amount grossed over the lifetime of the movie, given in
+	 * @param title    the Video Game's title
+	 * @param director the Video Game's director
+	 * @param year     the year the Video Game was released
+	 * @param gross    the amount grossed over the lifetime of the Video Game, given in
 	 *                 Millions of dollars
 	 */
 	public VideoGames(String serialNumber, String title, int year, String studio, String rating) {
 		this.serialNumber = serialNumber;
 		this.title = title;
 		this.year = year;
-		this.studioOrPublisher = studio;
+		this.publisher = studio;
 		this.rating = rating;
 	}
 
 	/**
-	 * Accesses the title of the Movie
+	 * Accesses the title of the Video Game
 	 *
-	 * @return the Movie's title
+	 * @return the Video Game's title
 	 */
 	public String getTitle() {
-		return title;
+		return this.title;
 	}
 
 	/**
-	 * Accesses the director of the Movie
+	 * Accesses the director of the Video Game
 	 *
-	 * @return the Movie's director
+	 * @return the Video Game's director
 	 */
 	public String getSerialNumber() {
-		return serialNumber;
+		return this.serialNumber;
 	}
 
 	/**
-	 * Access the year the Movie was released in theaters
+	 * Access the year the Video Game was released in theaters
 	 *
-	 * @return the year the Movie was released
+	 * @return the year the Video Game was released
 	 */
 	public int getYear() {
-		return year;
+		return this.year;
 	}
 
 	/**
-	 * Access the gross amount earned by the Movie in Millions of dollars
+	 * Access the gross amount earned by the Video Game in Millions of dollars
 	 *
-	 * @return the Movie's gross in Millions
+	 * @return the Video Game's gross in Millions
 	 */
-	public String getStudioOrPublisher() {
-		return studioOrPublisher;
+	public String getPublisher() {
+		return this.publisher;
 	}
 	
 	public String getRating() {
-		return rating;
+		return this.rating;
 	}
 
 	/**
-	 * Sets the title of the Movie
+	 * Sets the title of the Video Game
 	 *
-	 * @param title the Movie's title
+	 * @param title the Video Game's title
 	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
 	/**
-	 * Sets the director of the Movie
+	 * Sets the director of the Video Game
 	 *
-	 * @param director the Movie's director
+	 * @param director the Video Game's director
 	 */
 	public void setisbn(String serialNumber) {
 		this.serialNumber = serialNumber;
 	}
 
 	/**
-	 * Sets the year the Movie was released
+	 * Sets the year the Video Game was released
 	 *
-	 * @param year the year the Movie was released
+	 * @param year the year the Video Game was released
 	 */
 	public void setYear(int year) {
 		this.year = year;
 	}
 
 	/**
-	 * Sets the gross amount earned by the Movie in Millions of dolalrs
+	 * Sets the gross amount earned by the Video Game in Millions of dolalrs
 	 *
 	 * @param gross the gross amount in Millions of dollars
 	 */
 	public void setStudioOrPublisher(String studio) {
-		this.studioOrPublisher = studio;
+		this.publisher = studio;
 	}
 	
 	public void setRating(String rating) {
@@ -105,7 +104,7 @@ public class VideoGames {
 	}
 
 	/**
-	 * Creates a String of the Movie information the following format: Title:
+	 * Creates a String of the Video Game information the following format: Title:
 	 * <title> Director: <director> Year: <year> Gross in Millions: $<grossMillions>
 	 * Note that there should be no <> in the resulting String Also the
 	 * grossMillions should be displayed as follows: XXX,XXX.XX
@@ -113,17 +112,17 @@ public class VideoGames {
 	@Override
 	public String toString() {
 		String result = "";
-		result += "Serial Number: " + serialNumber + "\nTitle: " + title + "\nYear: " + year + "\nStudio or Publisher: "
-				+ studioOrPublisher + "\nRating: " + rating + "\n\n";
+		result += "Serial Number: " + serialNumber + "\nTitle: " + title + "\nYear: " + year + "\nPublisher: "
+				+ publisher + "\nRating: " + rating + "\n\n";
 		return result;
 	}
 
 	/**
-	 * Determines whether two Movie objects are equal by comparing titles and
+	 * Determines whether two Video Game objects are equal by comparing titles and
 	 * directors
 	 *
-	 * @param otherMovie the second Movie object
-	 * @return whether the Movies are equal
+	 * @param otherVideo Game the second Video Game object
+	 * @return whether the Video Games are equal
 	 */
 	@Override
 	public boolean equals(Object o) {
@@ -139,35 +138,35 @@ public class VideoGames {
 	}
 
 	/**
-	 * Compares two Movie objects to determine ordering Returns 0 if the two items
-	 * are equal Return -1 if this Movie's title comes alphabetically before the
-	 * other Movie's title. Returns 1 if the other Movie's title comes
-	 * alphabetically before this movie's title If the two movie's titles are the
+	 * Compares two Video Game objects to determine ordering Returns 0 if the two items
+	 * are equal Return -1 if this Video Game's title comes alphabetically before the
+	 * other Video Game's title. Returns 1 if the other Video Game's title comes
+	 * alphabetically before this Video Game's title If the two Video Game's titles are the
 	 * same, will differentiate by director's name (alphabetical comparison)
 	 *
-	 * @param the other Movie object to compare to this
-	 * @return 0 (same movie), -1 (this movie ordered first) or 1 (the other movie
+	 * @param the other Video Game object to compare to this
+	 * @return 0 (same Video Game), -1 (this Video Game ordered first) or 1 (the other Video Game
 	 *         ordered first)
 	 */
 	//@Override
-	public int compareTo(VideoGames otherMovie) {
-		if (this.equals(otherMovie)) {
+	public int compareTo(VideoGames otherVideoGame) {
+		if (this.equals(otherVideoGame)) {
 			return 0;
-		} else if (serialNumber.compareTo(otherMovie.serialNumber) < 0) {
+		} else if (serialNumber.compareTo(otherVideoGame.serialNumber) < 0) {
 			return -1;
-		} else if (serialNumber.compareTo(otherMovie.serialNumber) > 0) {
+		} else if (serialNumber.compareTo(otherVideoGame.serialNumber) > 0) {
 			return 1;
 		}
-		if (title.compareTo(otherMovie.title) < 0) {
+		if (title.compareTo(otherVideoGame.title) < 0) {
 			return -1;
-		} else if (title.compareTo(otherMovie.title) > 0) {
+		} else if (title.compareTo(otherVideoGame.title) > 0) {
 			return 1;
 		}
 		return 0;
 	}
 
 	/**
-	 * Returns a consistent hash code for each Movie by summing the Unicode values
+	 * Returns a consistent hash code for each Video Game by summing the Unicode values
 	 * of each character in the key Key = title + director
 	 *
 	 * @return the hash code
@@ -181,6 +180,8 @@ public class VideoGames {
 		}
 		return sum;
 	}
+	
+	
 
 
 }
