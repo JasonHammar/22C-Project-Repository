68b213df66c0102f1d44a91e043dd066e76c6ce3
zyadkhater <66536641+zyadkhater@users.noreply.github.com>
2020-06-17 11:52:06 -0700

    Update VideoGames2.java

--- VideoGames2.java
+++ VideoGames2.java
@@ -1,6 +1,6 @@
 import java.io.PrintWriter;
 
-public class VideoGames2 implements Comparable<VideoGames2> {
+public class VideoGames2 extends VideoGames implements Comparable<VideoGames2> {
 	private String serialNumber;
 	private String title;
 	private Integer year;
@@ -17,131 +17,10 @@ public class VideoGames2 implements Comparable<VideoGames2> {
 	 *                 in Millions of dollars
 	 */
 	public VideoGames2(String serialNumber, String title, int year, String studio, String rating) {
-		this.serialNumber = serialNumber;
-		this.title = title;
-		this.year = year;
-		this.publisher = studio;
-		this.rating = rating;
+		super(serialNumber, title, year, studio, rating);
 	}
 
-	/**
-	 * Accesses the title of the Video Game
-	 *
-	 * @return the Video Game's title
-	 */
-	public String getTitle() {
-		return this.title;
-	}
-
-	/**
-	 * Accesses the director of the Video Game
-	 *
-	 * @return the Video Game's director
-	 */
-	public String getSerialNumber() {
-		return this.serialNumber;
-	}
-
-	/**
-	 * Access the year the Video Game was released in theaters
-	 *
-	 * @return the year the Video Game was released
-	 */
-	public int getYear() {
-		return this.year;
-	}
-
-	/**
-	 * Access publisher's name who published the game
-	 *
-	 * @return the Video Game's publisher
-	 */
-	public String getPublisher() {
-		return this.publisher;
-	}
 	
-	/**
-	 * Access the rating of the video game
-	 *
-	 * @return the Video Game's rating
-	 */
-	public String getRating() {
-		return this.rating;
-	}
-
-	/**
-	 * Sets the title of the Video Game
-	 *
-	 * @param title the Video Game's title
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/**
-	 * Sets the serial number of the Video Game
-	 *
-	 * @param serial number the Video Game's serial number
-	 */
-	public void setSerialNumber(String serialNumber) {
-		this.serialNumber = serialNumber;
-	}
-
-	/**
-	 * Sets the year the Video Game was released
-	 *
-	 * @param year the year the Video Game was released
-	 */
-	public void setYear(int year) {
-		this.year = year;
-	}
-
-	/**
-	 * Sets the Studio or Publisher who made the video game
-	 *
-	 * @param Studio or publisher who made the video game
-	 */
-	public void setStudioOrPublisher(String studio) {
-		this.publisher = studio;
-	}
-
-	public void setRating(String rating) {
-		this.rating = rating;
-	}
-
-	/**
-	 * Creates a String of the Video Game information the following format: Serial Number:
-	 * <serialNumber> Title: <title> Year: <year> Publisher: <publisher> Rating: <rating>
-	 * Note that there should be no <> in the resulting String.
-	 */
-	@Override
-	public String toString() {
-		String result = "";
-		result += "Serial Number: " + serialNumber + "\nTitle: " + title + "\nYear: " + year + "\nPublisher: "
-				+ publisher + "\nRating: " + rating + "\n\n";
-		return result;
-	}
-
-	/**
-	 * Determines whether two Video Game objects are equal by comparing serialNumber
-	 * and Title.
-	 *
-	 * @param otherVideo Game the second Video Game object
-	 * @return whether the Video Games are equal
-	 */
-	@Override
-	public boolean equals(Object o) {
-		if (o == this) {
-			return true;
-		} else if (!(o instanceof VideoGames2)) {
-			return false;
-		} else {
-			VideoGames2 p = (VideoGames2) o;
-			return serialNumber.equals(p.serialNumber) && title.equals(p.title);
-		}
-
-	}
-
 	/**
 	 * Compares two Video Game objects to determine ordering Returns 0 if the two
 	 * items are equal Return -1 if this Video Game's serial number comes alphabetically
@@ -177,29 +56,6 @@ public class VideoGames2 implements Comparable<VideoGames2> {
         	return 1;
         }
  
-	}
-
-	/**
-	 * Returns a consistent hash code for each Video Game by summing the Unicode
-	 * values of each character in the key Key = serialNumber + title
-	 *
-	 * @return the hash code
-	 */
-	@Override
-	public int hashCode() {
-		String key = serialNumber + title;
-		int sum = 0;
-		for (int i = 0; i < key.length(); i++) {
-			sum += (int) key.charAt(i);
-		}
-		return sum;
-	}
-	/**
-	 * Prints all the games that are stored in hash table in original format
-	 */
-	public void printGameToFile(PrintWriter output) {
-		output.println(serialNumber + "\n" + title + "\n" 
-				+ publisher + "\n" + year + "\n" + rating + "\n");
 	}
 
 }
