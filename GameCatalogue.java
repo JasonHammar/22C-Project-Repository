0923117da15063de52fb0d7f05891ed2ebb8a10d
ar201 <66536556+ar201@users.noreply.github.com>
2020-06-17 15:23:32 -0700

    Update GameCatalogue.java

--- GameCatalogue.java
+++ GameCatalogue.java
@@ -9,6 +9,12 @@ public class GameCatalogue {
 	BST<VideoGames2> bst2 = new BST<>();
 
 	public static void main(String[] args) throws IOException {
+		String serialCode;
+		String title;
+		int year;
+		String publisher;
+		String rating;
+
 		GameCatalogue gameCatalogue = new GameCatalogue();
 		Scanner input = new Scanner(System.in);
 
@@ -19,40 +25,182 @@ public class GameCatalogue {
 		String fileName;
 		while (isTrue) {
 			try {
-				System.out.println("Enter text file name");
+				System.out.print("Enter text file name: ");
 				fileName = input.nextLine();
 				gameCatalogue.readFile(fileName);
 				isTrue = false;
 			} catch (IOException e) {
-				System.out.println(e.getMessage());
+				System.out.println("Not a valid file name");
+			}
+		}
+
+		int choice = 0;
+		VideoGames vg;
+		VideoGames2 vg2;
+
+		System.out.println();
+
+		while (choice != 6) {
+			System.out.println("Please select one of the following:");
+			System.out.println("1. Print your catalog");
+			System.out.println("2. Search for video game");
+			System.out.println("3. Add video game");
+			System.out.println("4. Remove video game");
+			System.out.println("5. Save to file");
+			System.out.println("6. Quit");
+			System.out.print("Enter your choice: ");
+			choice = input.nextInt();
+
+			while (choice < 1 || choice > 6) {
+				System.out.println("\nInvalid choice!");
+				System.out.print("Enter your choice: ");
+				choice = input.nextInt();
+			}
+
+			if (choice == 1) {
+				System.out.println("\nPlease select one of the following:");
+				System.out.println("1. Print unsorted");
+				System.out.println("2. Print sorted by serial code");
+				System.out.println("3. Print sorted by title");
+				System.out.print("Enter your choice: ");
+				choice = input.nextInt();
+
+				while (choice < 1 || choice > 3) {
+					System.out.println("\nInvalid choice!");
+					System.out.print("Enter your choice: ");
+					choice = input.nextInt();
+				}
+
+				if (choice == 1) {
+					System.out.println();
+					System.out.print(gameCatalogue.hash);
+				}
+
+				else if (choice == 2) {
+					System.out.println();
+					gameCatalogue.bst.inOrderPrint();
+				}
+
+				else {
+					System.out.println();
+					gameCatalogue.bst2.inOrderPrint();
+				}
+			}
+
+			else if (choice == 2) {
+				System.out.println("\nPlease select one of the following:");
+				System.out.println("1. Search by serial code");
+				System.out.println("2. Search by title");
+				System.out.print("Enter your choice: ");
+				choice = input.nextInt();
+
+				while (choice < 1 || choice > 2) {
+					System.out.println("\nInvalid choice!");
+					System.out.print("Enter your choice: ");
+					choice = input.nextInt();
+				}
+
+				if (choice == 1) {
+					System.out.print("\nEnter the serial code: ");
+					serialCode = input.next();
+					vg = new VideoGames(serialCode, "", 0, "", "");
+
+					if (gameCatalogue.bst.search(vg) == false) {
+						System.out.println("\nVideo game with serial code: " + serialCode + " is not in the database\n");
+					}
+
+					else {
+						System.out.println("\nVideo game with serial code: " + serialCode + " is in the database\n");
+						System.out.println("Displaying search result:\n");
+						gameCatalogue.bst.printElement(vg);
+					}
+				}
+
+				else {
+					System.out.print("\nEnter the title: ");
+					title = input.next() + input.nextLine();
+					vg2 = new VideoGames2("", title, 0, "", "");
+
+					if (gameCatalogue.bst2.search(vg2) == false) {
+						System.out.println("\nVideo game with title: " + title + " is not in the database\n");
+					}
+
+					else {
+						System.out.println("\nVideo game with title: " + title + " is in the database\n");
+						System.out.println("Displaying search results:\n");
+						gameCatalogue.bst2.printAllMatches(vg2);
+					}
+				}
+			}
+
+			else if (choice == 3) {
+				System.out.print("\nEnter the serial code: ");
+				serialCode = input.next();
+				System.out.print("Enter the title: ");
+				title = input.next() + input.nextLine();
+				System.out.print("Enter the year: ");
+				year = input.nextInt();
+				System.out.print("Enter the publisher: ");
+				publisher = input.next();
+				System.out.print("Enter the rating: ");
+				rating = input.next();
+
+				vg = new VideoGames(serialCode, title, year, publisher, rating);
+				vg2 = new VideoGames2(serialCode, title, year, publisher, rating);
+				
+				gameCatalogue.hash.insert(vg);
+				gameCatalogue.bst.insert(vg);
+				gameCatalogue.bst2.insert(vg2);
+
+				System.out.println("\n" + title + " was added!\n");
+			}
+
+			else if (choice == 4) {
+				System.out.print("\nEnter the serial code: ");
+				serialCode = input.next();
+				System.out.print("Enter the title: ");
+				title = input.next() + input.nextLine();
+
+				vg = new VideoGames(serialCode, title, 0, "", "");
+				vg2 = new VideoGames2(serialCode, title, 0, "", "");
+
+				if (gameCatalogue.hash.search(vg) == -1) {
+					System.out.println("\nI cannot find " + title + " with serial code: " 
+							   + serialCode + " in the database.\n");
+				}
+
+				else {
+					gameCatalogue.hash.remove(vg);
+					gameCatalogue.bst.remove(vg);
+					gameCatalogue.bst2.remove(vg2);
+
+					System.out.println("\n" + title + " was removed!\n");
+				}
+			}
+
+			else {
+				gameCatalogue.writeFile();
+
+				if (choice == 6) {
+					System.out.print("\nGoodbye!");
+					break;
+				}
+
+				System.out.println("\nCatalogue is saved in out.txt!");
+				System.out.println("Your current catalogue:\n");
+				System.out.print(gameCatalogue.hash);
 			}
 		}
 		input.close();
-		// printing out by sorted primary
-		System.out.println("****Primary Sort****");
-		gameCatalogue.bst.inOrderPrint();
-
-		// printing out by sorted secondary
-		System.out.println("****Secondary Sort****");
-		gameCatalogue.bst2.inOrderPrint();
-
-		// writing file to out.txt
-		gameCatalogue.writeFile();
-
 	}
 
 	/**
 	 * reads file content
 	 * 
-	 * @precondition fileName exists
-	 * @throws IOException if file does not exist
+	 * @throws IOException
 	 */
 	public void readFile(String fileName) throws IOException {
 		File inFile = new File(fileName);
-
-		if (!inFile.exists()) {
-			throw new IOException("readFile cannot find the file");
-		}
 		Scanner in = new Scanner(inFile);
 
 		while (in.hasNext()) {
@@ -69,6 +217,7 @@ public class GameCatalogue {
 
 			VideoGames v = new VideoGames(serialNum, title, year, publisher, rating);
 			VideoGames2 v2 = new VideoGames2(serialNum, title, year, publisher, rating);
+			
 			hash.insert(v);
 			bst.insert(v);
 			bst2.insert(v2);
@@ -77,7 +226,7 @@ public class GameCatalogue {
 	}
 
 	/**
-	 * 
+	 * outputs the hash contents onto file by calling on hash.printOut
 	 */
 	public void writeFile() throws IOException {
 		File outFile = new File("out.txt");
@@ -85,5 +234,4 @@ public class GameCatalogue {
 		hash.printOut(output);
 		output.close();
 	}
-
 }
