958d65f2117935099194041e8a9f9bbe8b792b2d
zyadkhater <66536641+zyadkhater@users.noreply.github.com>
2020-06-19 14:15:48 -0700

    Update VideoGames.java

--- VideoGames.java
+++ VideoGames.java
@@ -1,3 +1,14 @@
+/*
+* VideoGames.java
+* @author Daniel Morales
+* @author Jason Hammar
+* @author Zyad Khater
+* @author Zeko Khater
+* @author Donggyu(Alex) Yu
+* @author Aurielle Jocom
+* CIS 22C, Course Project
+*/
+
 import java.io.PrintWriter;
 
 public class VideoGames implements Comparable<VideoGames> {
